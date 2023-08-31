981a4f23fa4e55dfdea59b675f36656720bea5b3
Carlos Franco <cf9220543@gmail.com>
2023-08-31 15:35:11 -0500

    "En este codigo se cambio el hecho de sumar la letra para restarlo para que en lugar de codificar el codigo, lo decodifique, ingresando el mensaje codificado anteriormente y muestre el mensaje original"

---
+++ Decodificador.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Decodificador {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese mensaje a decodificar: ");
+        String mensaje = sc.next();
+        System.out.println("Cuántas veces vas a mover la letra");
+        int veces = sc.nextInt();
+
+        String mensajeDecodificado = "";
+
+        for(int i = 0; i < mensaje.length(); i++) {
+            char letra = mensaje.charAt(i);
+            boolean minuscula = (letra >= 'a' && letra <='z');
+            boolean mayuscula = (letra >= 'A' && letra <='Z');
+            if(!(minuscula || mayuscula))
+                mensajeDecodificado += letra;
+            else {
+                int ascii = (int)letra;
+                int AsciiBase = (int)'a';
+                if(mayuscula)
+                    AsciiBase = (int)'A';
+                int nuevoAscii = (ascii - AsciiBase - veces) % 26 + AsciiBase;
+                char nuevaLetra = (char)nuevoAscii;
+                mensajeDecodificado += nuevaLetra;
+            }
+        }
+        System.out.println(mensajeDecodificado);
+    }
+}
